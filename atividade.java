0ccd3009f86bda826b6f158989cd250dd97f6be9
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-19 19:41:04 -0300

    Add files via upload

---
+++ atividade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; 
+
+ public class atividade { 
+
+   public static void main(String[] args) { 
+    Scanner entrada = new Scanner(System.in);
+        
+        int nota;
+        System.out.println("digite um valor de 0 a 10: ");
+        do {
+          
+           nota = entrada.nextInt();
+           if (nota <=10 && nota >=0)
+           {
+            System.out.println("Muito Bem! \n voce digitou a nota: "+ nota);}
+            else{
+             System.out.println("Nota invalida, Digite um numero entre 0 e 10: ");
+            }
+           
+        } while (nota >10 || nota <0);
+        
+        entrada.close();
+    }
+}
+  
