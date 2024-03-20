c1c065111dbb86b6a42074d7d52a27b5253c8eb2
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-20 20:44:41 -0300

    Add files via upload

---
+++ atividade4.java
@@ -0,0 +1,18 @@
+
+public class atividade4 {
+    public static void main (String[] args){
+double paisA = 80000;
+double paisB = 200000;
+double taxaB = 0.015;
+double taxaA = 0.03;
+int anos = 0;
+do {
+    paisA *= (1 + taxaA);
+    paisB *= (1 + taxaB);
+     anos++;
+}while (paisA < paisB);
+
+ System.out.println("Levariam aproximadamente "+anos+" anos para atingirem a mesma populaçao! ");
+
+}
+}
