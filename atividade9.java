cf9ae5d847d1584fccda348948009221e1903418
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-22 10:40:36 -0300

    Add files via upload

---
+++ atividade9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class atividade9 {
+    public static void main(String[] args) {
+Scanner entrada = new Scanner(System.in);
+for(int i=0;i<=50;i++){
+    int numero = i%2; 
+    if (numero!=0)
+    System.out.println(i);
+
+
+}
+
+    }
+}
