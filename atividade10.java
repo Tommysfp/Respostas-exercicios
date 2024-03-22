31142e6e19e56a6f0c6e39ed2cc32a9b1bc17678
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-22 10:54:09 -0300

    Add files via upload

---
+++ atividade10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class atividade10 {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+System.out.println("Numero 1");
+int numero1 = entrada.nextInt();
+System.out.println("Numero 2");
+int numero2 = entrada.nextInt();
+
+for(int i=numero1+1;i<numero2;i++){
+    System.out.println(i);
+  }
+ }
+}
