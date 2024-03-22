04163bedf4d8496872b92d797126b684247a13e9
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-21 23:04:25 -0300

    Add files via upload

---
+++ atividade6.java
@@ -0,0 +1,11 @@
+public class atividade6 {
+    public static void main(String[] args){
+
+System.out.println("Numeros de 1 a 20 um ao lado do outro:");
+
+for(int i = 1; i <= 20; i++) {
+    System.out.print(i+", ");
+}
+
+    }
+}
