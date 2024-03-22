194ea743ed7a4f67a263729387a3b7a0bf8a14eb
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-22 11:28:59 -0300

    Add files via upload

---
+++ atividade12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class atividade12 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+int numero;
+int tabuada;
+int i;
+    System.out.println("Digite seu numero: ");
+     numero = entrada.nextInt();  
+     System.out.println("Tabuada de "+numero+":"); 
+for(i=1;i<=10;i++){
+tabuada = numero*i;
+System.out.println(numero+" x "+i+" = "+tabuada);
+}
+
+
+entrada.close();
+    }
+}
