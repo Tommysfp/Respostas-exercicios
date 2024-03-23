4ffc4a765673347e79843c8a8faab3e81fe7a72e
Tommysfp <164034156+Tommysfp@users.noreply.github.com>
2024-03-23 20:34:57 -0300

    Update and rename atividade9.java to atividade09.java

--- atividade09.java
+++ atividade09.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class atividade9 {
+public class atividade09 {
     public static void main(String[] args) {
 Scanner entrada = new Scanner(System.in);
 for(int i=0;i<=50;i++){
